5e21acd5591cbbed71d712bf09dde12de9a327c3
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-12-03 04:28:21 +0900

    📄️ :: 페이지가 하단바와 동일해지도록 구현

--- app/src/main/java/com/g3c1/myapplication/MainActivity.kt
+++ app/src/main/java/com/g3c1/myapplication/MainActivity.kt
@@ -3,15 +3,26 @@ package com.g3c1.myapplication
 import BaseActivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.g3c1.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun viewSetting() {
         binding.activity = this
+        fragmentSetting()
     }
 
     override fun observeEvent() {
 
+    }
+
+    private fun fragmentSetting() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
+        val navController = navHostFragment.findNavController()
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 
 }
